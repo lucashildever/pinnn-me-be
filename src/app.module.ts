f1811faf109452975f1d58e7ff18fdbea1c3e7e2
lucashildever <lucaspaulo_212@hotmail.com>
2025-05-22 19:49:34 -0300

    typeorm added and providers registered

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import configuration from 'config/configuration';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from './auth/auth.module';
+import { MuralModule } from './mural/mural.module';
+import { CollectionModule } from './collection/collection.module';
+import { PinGroupPageModule } from './pin-group-page/pin-group-page.module';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.ENVIRONMENT?.toLowerCase() === 'development',
+    }),
+    MuralModule,
+    CollectionModule,
+    PinGroupPageModule,
+    AuthModule,
+    UserModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
