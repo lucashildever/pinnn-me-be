e70b29e2c9ac4a720916b186ac60e37ceae5d2f0
lucashildever <lucaspaulo_212@hotmail.com>
2025-07-03 14:14:41 -0300

    added caching

--- config/configuration.ts
+++ config/configuration.ts
@@ -4,8 +4,13 @@ export default () => ({
   database: {
     port: parseInt(process.env.DB_PORT ?? '3306', 10),
     host: process.env.DB_HOST,
-    name: process.env.DV_NAME,
+    name: process.env.DB_NAME,
     pass: process.env.DB_PASS,
     user: process.env.DB_USER,
+  },
+  redis: {
+    host: process.env.REDIS_HOST ?? 'localhost',
+    port: parseInt(process.env.REDIS_PORT ?? '6379', 10),
+    ttl: parseInt(process.env.REDIS_TTL ?? '60', 10),
   },
 });
