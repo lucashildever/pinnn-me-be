50b0eea5efb34bd62d8c06b92f57fa8c89d7751e
lucashildever <lucaspaulo_212@hotmail.com>
2025-07-03 15:03:42 -0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
