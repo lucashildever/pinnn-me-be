decec42f3a05b804f6b3bd9482590f75e27040bf
lucashildever <lucaspaulo_212@hotmail.com>
2025-07-19 21:42:15 -0300

    refactor: webhook service imported in payments module

--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhooksService } from './webhooks.service';
 import { WebhooksController } from './webhooks.controller';
+import { PaymentsModule } from 'src/payments/payments.module';
 
 @Module({
+  imports: [PaymentsModule],
   controllers: [WebhooksController],
   providers: [WebhooksService],
 })
