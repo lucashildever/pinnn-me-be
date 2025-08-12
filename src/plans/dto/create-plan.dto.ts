465b76317bc593c0d6bf945e390e363562805a1e
lucashildever <lucaspaulo_212@hotmail.com>
2025-08-11 21:53:54 -0300

    feat: add class-validators

--- src/plans/dto/create-plan.dto.ts
+++ src/plans/dto/create-plan.dto.ts
@@ -1,10 +1,43 @@
+import {
+  IsArray,
+  IsObject,
+  IsString,
+  IsBoolean,
+  IsOptional,
+  IsNotEmpty,
+  ArrayNotEmpty,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
 import { CreatePriceDto } from './create-price.dto';
 
-export interface CreatePlanDto {
+export class CreatePlanDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   slug: string;
+
+  @IsBoolean()
+  @IsOptional()
   isDefault?: boolean;
+
+  @IsArray()
+  @IsNotEmpty()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
   features: string[];
+
+  @IsObject()
+  @IsNotEmpty()
   limits: Record<string, any>;
+
+  @IsArray()
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => CreatePriceDto)
   prices: CreatePriceDto[];
 }
--- src/plans/dto/create-price.dto.ts
+++ src/plans/dto/create-price.dto.ts
@@ -1,5 +1,19 @@
-export interface CreatePriceDto {
+import { IsIn, IsString, IsNumber, IsNotEmpty } from 'class-validator';
+import { ISO_CURRENCIES } from '../constants/currencies.constant';
+
+export class CreatePriceDto {
+  @IsNumber()
+  @IsNotEmpty()
   price: number;
+
+  @IsString()
+  @IsIn(['monthly', 'yearly'])
   billingPeriod: string;
+
+  @IsString()
+  @IsIn(ISO_CURRENCIES, {
+    each: true,
+    message: `Each currency must be one of: ${ISO_CURRENCIES.join(', ')}`,
+  })
   currency: string;
 }
