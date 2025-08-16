38e64b8a9c54078a2abfbbf7d5257cc70cb25064
lucashildever <lucaspaulo_212@hotmail.com>
2025-08-16 15:12:12 -0300

    feat: configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +25,29 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API de Exemplo')
+    .setDescription('Documentação da API usando Swagger')
+    .setVersion('1.0')
+    .addTag('users', 'Operações relacionadas a usuários') // Edit this!
+    .addTag('products', 'Operações relacionadas a produtos') // Edit this!
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  const port = process.env.PORT || 4000;
+
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Swagger available at: http://localhost:${port}/api-docs`);
 }
 bootstrap();
